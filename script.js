16e4c5267d7cf2abe3f5ebe1e0df129e55256d5b
Anchovys <hakker990@gmail.com>
2020-03-15 22:21:47 +0300

    refactor

--- script.js
+++ script.js
@@ -4,17 +4,26 @@ let itemScalePx = 32;       // a size for object (32x32px default)
 let playerX  = getRandomInt(4);
 let score = 0;        // current score
 let pause = true;     // game in pause
+let dy = 2;             // rail offset
+
+let rockY = -itemScalePx; // current position for rock
+let rockX = getRandomInt(4);
+
+let diamondY = itemScalePx; // current position for diamond
+let diamondX = getRandomInt(4);
+
+let rockId = getRandomInt(2); // rock texture id
 
 function loadImages(sources, callback)
 {
     let images = {};
     let loadedImages = 0;
     let numImages = 0;
-    for(var src in sources)
+    for(let src in sources)
     {
       numImages++;
     }
-    for(var src in sources)
+    for(let src in sources)
     {
       images[src] = new Image();
       images[src].onload = function()
@@ -28,7 +37,7 @@ function loadImages(sources, callback)
     }
   }
 
-  var sources =
+  let sources =
   {
     rail:     'https://anchovys.github.io/diamond-rails/tex/rail.png',
     rock0:    'https://anchovys.github.io/diamond-rails/tex/rock0.png',
@@ -44,7 +53,7 @@ function loadImages(sources, callback)
 
 document.addEventListener('keydown', function(event)
 {
-    switch (event.code )
+    switch (event.code)
     {
         case 'KeyA':        //  нажатие влево
         case 'ArrowLeft':
@@ -61,10 +70,11 @@ document.addEventListener('keydown', function(event)
         case 'KeyP':        // установка/снятие паузы только на кнопку P
              pause = !pause;
             break;
+    }
 
-        default:            // снятие с паузы на любую другую кнопку
-              pause = false;
-            break;
+    if(pause && event.code !== 'KeyP') // снятие с паузы на любую другую кнопку
+    {
+        pause = false;
     }
 });
 
@@ -96,81 +106,88 @@ document.addEventListener("DOMContentLoaded", function ()
         ClickAt(e.clientX - rect.left, e.clientY - rect.top, rect.width);
     };
 
+    // for text
     cnvContext.canvas.width = itemScalePx * 4;
     cnvContext.canvas.height = (itemScalePx-1) * 4;
     cnvContext.font = "20px Arial";
-    cnvContext.fillStyle = "#84ffff";
+    cnvContext.fillStyle = '#84ffff';
     cnvContext.textAlign = "center";
 
-    let dy = 2;
-    let rockY = -itemScalePx;
-    let diamondY = itemScalePx;
-    let rockX = getRandomInt(4);
-    let diamondX = getRandomInt(4);
-
-    let rockId = getRandomInt(2);
-
-    setInterval(function()
-    {
-        loadImages(sources, function(images)
-        {
-            cnvContext.clearRect(0, 0, canvas.width, canvas.height);
-
-            for(let x = 0; x < 4; x++)
-            {   for(let y = 0; y < 4; y++)
-                {
-                    cnvContext.drawImage(images.rail, x * itemScalePx, y * itemScalePx + dy - 2, itemScalePx, itemScalePx);
-                }
-            }
-
-            cnvContext.drawImage(images.player, playerX * itemScalePx, 2 * itemScalePx, itemScalePx, itemScalePx);
-            cnvContext.drawImage((rockId === 0) ? images.rock0 : images.rock1, rockX * itemScalePx, rockY, itemScalePx, itemScalePx);
-            cnvContext.drawImage(images.diamond, diamondX * itemScalePx, diamondY, itemScalePx, itemScalePx);
-
-            cnvContext.fillText(score, (playerX * itemScalePx) + itemScalePx/2, (3 * itemScalePx) - itemScalePx / 4);
-
-        });
-
-        if (pause) return;
-
-        rockY    += 3;
-        diamondY += 2;
-
-
-        if(rockY > canvas.height)
-        {
-            rockX = getRandomInt(4);
-            rockY = -itemScalePx;
-            rockId = getRandomInt(2);
-        }
-        if(diamondY > canvas.height || (rockY + itemScalePx/2 > diamondY && rockY < diamondY + itemScalePx/2 && rockX === diamondX))
-        {
-            diamondX = getRandomInt(4);
-            diamondY = -itemScalePx;
-        }
-
-        dy = -dy;
-
-        if(rockY + itemScalePx > 2 * itemScalePx && rockY < 3 * itemScalePx && rockX === playerX)
-        {
-            //alert("You lose. Score: " + score);
-            rockX       = getRandomInt(4);
-            rockY       = -itemScalePx;
-            diamondX    = getRandomInt(4);
-            diamondY    = -itemScalePx;
-            rockId      = getRandomInt(2);
-            playerX     = getRandomInt(4);
-            score       = 0;
-            dy          = 2;
-            pause       = true;
-        }
-        if(diamondY + itemScalePx > 2 * itemScalePx && diamondY < 3 * itemScalePx && diamondX === playerX)
-        {
-            score++;
-            diamondX = getRandomInt(4);
-            diamondY = -itemScalePx;
-        }
-
-
-    }, 50);
+    // run main update-loop
+    setInterval(update, 50);
 });
+
+function update() // update loop
+{
+    loadImages(sources, function(images)
+    {
+        // clear the rect
+        cnvContext.clearRect(0, 0, canvas.width, canvas.height);
+
+        // drawing rails
+        for(let x = 0; x < 4; x++)
+        for(let y = 0; y < 4; y++)
+            cnvContext.drawImage(images.rail, x * itemScalePx, y * itemScalePx + dy - 2, itemScalePx, itemScalePx);
+
+        // drawing player
+        cnvContext.drawImage(images.player, playerX * itemScalePx, 2 * itemScalePx, itemScalePx, itemScalePx);
+
+        // drawing text
+        cnvContext.fillText(score, (playerX * itemScalePx) + itemScalePx/2, (3 * itemScalePx) - itemScalePx / 4);
+
+        // drawing rock
+        cnvContext.drawImage((rockId === 0) ? images.rock0 : images.rock1, rockX * itemScalePx, rockY, itemScalePx, itemScalePx);
+
+        // drawing diamond
+        cnvContext.drawImage(images.diamond, diamondX * itemScalePx, diamondY, itemScalePx, itemScalePx);
+    });
+
+    // exit from function if in pause
+    if (pause) return;
+
+    rockY    += 3; // move rock in 3px by Y
+    diamondY += 2; // move diamond in 2px by Y
+    dy = -dy;      // move rails
+
+    // respawn rock
+    if(rockY > canvas.height)
+        respawnRock();
+
+    // Respawn diamond
+    if(diamondY > canvas.height || (rockY + itemScalePx/2 > diamondY && rockY < diamondY + itemScalePx/2 && rockX === diamondX))
+        respawnDiamond();
+
+    // game over, respawn all
+    if(rockY + itemScalePx > 2 * itemScalePx && rockY < 3 * itemScalePx && rockX === playerX)
+    {
+        respawnRock();
+        respawnDiamond();
+        respawnPlayer();
+    }
+
+    // take diamond
+    if(diamondY + itemScalePx > 2 * itemScalePx && diamondY < 3 * itemScalePx && diamondX === playerX)
+    {
+        score++;
+        respawnDiamond();
+    }
+}
+
+function respawnRock()
+{
+    rockX = getRandomInt(4);
+    rockY = -itemScalePx;
+    rockId = getRandomInt(2);
+}
+
+function respawnDiamond()
+{
+    diamondX = getRandomInt(4);
+    diamondY = -itemScalePx;
+}
+
+function respawnPlayer() {
+    score       = 0;
+    dy          = 2;
+    pause       = true;
+}
